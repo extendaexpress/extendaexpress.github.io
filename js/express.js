cc729b7f8c0d874bfd0392acd9bf08b99af0f6c1
Tobias Munter <tobias@muntr.com>
2016-05-19 14:05:03 +0200

    updates.

--- js/express.js
+++ js/express.js
@@ -12,8 +12,10 @@ $(function() {
 $(window).scroll(function() {
     if ($(".navbar").offset().top > 50) {
         $(".navbar-fixed-top").addClass("top-nav-collapse");
+        $(".expresslogo").addClass("expresslogo-shown");
     } else {
         $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        $(".expresslogo").removeClass("expresslogo-shown");
     }
 });
 
@@ -22,7 +24,7 @@ $(function() {
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
+            scrollTop: $($anchor.attr('href')).offset().top + 150
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
